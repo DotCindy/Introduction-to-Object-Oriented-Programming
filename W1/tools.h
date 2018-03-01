2218a15714cf6b5c2f7852261d6a8460d0f970ad
Cindy Le <36822198+DotCindy@users.noreply.github.com>
2018-02-28 19:34:00 -0500

    Create tools.h

---
+++ W1/tools.h
@@ -0,0 +1,17 @@
+/*
+Name:                   Cindy Le
+Email:                  cle15@myseneca.ca
+Date:                   January 23, 2018
+Workshop:               At Home
+Description:            Compiling Modules
+*/
+
+#ifndef SICT_TOOLS_H
+#define SICT_TOOLS_H
+
+namespace sict {
+	int menu();
+	int getInt(int min, int max);
+}
+
+#endif
